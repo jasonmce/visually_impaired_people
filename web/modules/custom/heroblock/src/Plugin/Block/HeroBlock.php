b59b2fbdafb7582dd2b2ec7e21d916c7ffe0a127
JasonMcE <bitbucket@brightshinyobject.com>
2024-03-20 16:28:33 -0400

    Initial commit

---
+++ web/modules/custom/heroblock/src/Plugin/Block/HeroBlock.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\heroblock\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hero' Block.
+ *
+ * @Block(
+ *   id = "hero_block",
+ *   admin_label = @Translation("Hero block"),
+ *   category = @Translation("Hero World"),
+ * )
+ */
+class HeroBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $site_config = \Drupal::config('system.site');
+
+    $siteName = $site_config->get('name');
+    $siteSlogan = $site_config->get('slogan');
+
+    $imageElement = "<img src='themes/custom/vip_olivero_sub_theme/images/William.jpeg' />";
+    $nameElement = "<p class='site-title'><a href='/' title='Home'>$siteName</a></p>";
+    $sloganElement = "<p class='site-slogan'>$siteSlogan</p>";
+
+    return [
+      '#prefix' => "<div class='heroblock'>",
+      '#suffix' => '</div>',
+      '#markup' => $imageElement . $nameElement . $sloganElement,
+      '#attached' => ['library' => ['heroblock/heroblock']],
+    ];
+  }
+
+}
